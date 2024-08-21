ccaf8135467853ba16820a79706cfc380f55c615
sharmar1 <rohit.sharma@autodesk.com>
2024-08-21 23:36:55 +0530

    REST API Project

---
+++ models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Boolean, Column, Integer, String
+from database import Base, engine
+
+# Task 2 - Code Here
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    gender = Column(String)
+    country = Column(String)
+    is_active = Column(Boolean)
+    hashed_password = Column(String)
+
+Base.metadata.create_all(engine)
---
+++ schemas.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class UserBase(BaseModel):
+    email: str
+    username: str
+    first_name: str
+    last_name: str
+    gender: str
+    country: str
+    is_active: bool
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    class Config:
+        orm_mode = True
+
